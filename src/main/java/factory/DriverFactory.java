4b09aca900fb1c68cb94c2236b74fbb2a4cf851a
Trịnh Đăng Hùng <trinhdanghung7404@gmail.com>
2026-03-13 16:17:39 +0700

    add github actions workflow

--- src/main/java/factory/DriverFactory.java
+++ src/main/java/factory/DriverFactory.java
@@ -2,16 +2,23 @@ package factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
     public static WebDriver initDriver(){
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        // chạy headless cho CI
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
 
         return driver;
     }
-
 }
