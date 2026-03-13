1c92af24a3c52d9479f807ff456b2053a3c937ef
trinhdanghung7404-code <trinhdanghung7404@gmail.com>
2026-03-13 18:06:27 +0700

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,21 +1,12 @@
-package pages;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import utils.WaitHelper;
-
 public class HomePage {
 
     WebDriver driver;
     WaitHelper wait;
 
-    By loginBtn = By.cssSelector("a[href='https://practice.qabrains.com/']");
-    By registerBtn = By.cssSelector("a[href='https://practice.qabrains.com/registration']");
-    By forgotpassBtn = By.cssSelector("a[href='https://practice.qabrains.com/forgot-password']");
-    By formsubBtn = By.cssSelector("a[href='https://practice.qabrains.com/form-submission']");
+    By loginBtn = By.linkText("Login");
+    By registerBtn = By.linkText("Register");
+    By forgotpassBtn = By.linkText("Forgot Password");
+    By formsubBtn = By.linkText("Form Submission");
 
     public HomePage(WebDriver driver){
         this.driver = driver;
@@ -23,12 +14,12 @@ public class HomePage {
     }
 
     private void clickElement(By locator){
-    
+
         WebElement element = wait.waitForElementClickable(locator);
-    
+
         ((JavascriptExecutor) driver)
                 .executeScript("arguments[0].scrollIntoView({block:'center'});", element);
-    
+
         try{
             element.click();
         }
@@ -38,35 +29,19 @@ public class HomePage {
         }
     }
 
-    private void switchToNewWindow(){
-
-        String currentWindow = driver.getWindowHandle();
-
-        for(String window : driver.getWindowHandles()){
-            if(!window.equals(currentWindow)){
-                driver.switchTo().window(window);
-                break;
-            }
-        }
-    }
-
     public void openLoginPage(){
         clickElement(loginBtn);
-        switchToNewWindow();
     }
 
     public void openRegisterPage(){
         clickElement(registerBtn);
-        switchToNewWindow();
     }
 
     public void openForgotPasswordPage(){
         clickElement(forgotpassBtn);
-        switchToNewWindow();
     }
 
     public void openFormSubmissionPage(){
         clickElement(formsubBtn);
-        switchToNewWindow();
     }
 }
